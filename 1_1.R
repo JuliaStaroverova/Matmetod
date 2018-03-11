0096c37bd7cff4bbf35a774d1584a7259787a907
JuliaStaroverova <jul.staroverova@yandex.ru>
2018-03-11 17:53:22 +0400

    123

---
+++ 1_1.R
@@ -0,0 +1,8 @@
+rm(list=ls())
+iris[iris$Species==c("setosa","virginica"),]
+names(iris)
+Species
+iris[iris$Species=="Species"| iris$Species=="virginica",c("Petal.Length","Sepal.Width","Petal.Length","Petal.Width")]
+x[iris$Petal.Length, c("Petal.Length")]
+length(x)
+x

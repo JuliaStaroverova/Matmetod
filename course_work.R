c700d397cf59b4be1c9bc8b5a7371b3080597e4e
JuliaStaroverova <jul.staroverova@yandex.ru>
2018-03-11 19:42:57 +0400

    курсовая. код без данных а так все ок

---
+++ course_work.R
@@ -0,0 +1,24 @@
+#Линейная регрессия (Linear regression) — используемая в статистике регрессионная модель зависимости одной (объясняемой, зависимой) переменной y от другой или нескольких других переменных факторов, регрессоров, независимых переменных) x с линейной функцией зависимости.
+rm(list=ls())
+data(longley)
+setwd("C:/rdata")
+adat=read.csv("filename.csv", header=TRUE, sep=";", dec=",")# нет данных- нет файла
+fit <- lm(data$ROSN ~ data$BRN )
+summary(fit)
+model <- lm(data$y~data$x)
+plot(fit)
+
+
+#abline(model)
+
+
+# fit model
+#fit <- lm(Employed~., longley)
+# summarize the fit
+#summary(fit)
+# make predictions
+#predictions <- predict(fit, longley)
+# summarize accuracy
+#rmse <- mean((longley$Employed - predictions)^2)
+#print(rmse)
+#plot(fit)

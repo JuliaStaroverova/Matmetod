460cf74dc4aee6cbd7e051b8900c392f6985b28d
JuliaStaroverova <jul.staroverova@yandex.ru>
2018-03-11 17:53:22 +0400

    закончен

---
+++ 1_1.R
@@ -0,0 +1,20 @@
+rm(list=ls())
+iris[iris$Species==c("setosa","virginica"),]
+names(iris)
+a=iris[iris$Species=="setosa" | iris$Species=="virginica",c("Sepal.Length")]
+  A=mean(a)
+  #A
+b=iris[iris$Species=="setosa" | iris$Species=="virginica",c("Sepal.Width")]
+  B=mean(b)
+  #B
+c=iris[iris$Species=="setosa" | iris$Species=="virginica",c("Petal.Length")]
+  C=mean(c)
+  #C
+d=iris[iris$Species=="setosa" | iris$Species=="virginica",c("Petal.Width")]
+  D=mean(d)
+  #D
+x=c(A,B,C,D)
+names(x)<-c("Sepal.Length", "Sepal.Width",  "Petal.Length", "Petal.Width")
+sort(x)
+list(sort(x))
+
